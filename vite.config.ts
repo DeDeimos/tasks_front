cdb5a1ca06411e8d97c0bcb2a531bb1d953fac49
DeDeimos <rodion.tolkachev.03@inbox.ru>
2023-11-13 13:42:48 +0300

    action2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/task_front/',
   plugins: [react()],
-  base: '/task_front/'
+  
 })
