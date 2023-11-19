0d6cfc940e20fca4ea7c8b26ec6f39842d2c89bf
DeDeimos <rodion.tolkachev.03@inbox.ru>
2023-11-19 23:28:44 +0300

    action3

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,30 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/task_front/',
   plugins: [react()],
-  
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@app": "/src/app",
+      "@assets": "/src/assets",
+      "@pages": "/src/pages",
+      "@widgets": "/src/widgets",
+      "@features": "/src/features",
+      "@entities": "/src/entities",
+      "@shared": "/src/shared",
+    },
+  },
 })
